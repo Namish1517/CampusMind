fe32da3294b494eff53beecb16453b0644793b12
Namish1517 <namish1517@gmail.com>
2026-03-11 01:52:47 +0530

    update API URL for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const moodRoutes = require("./routes/moodRoutes");
-
+const PORT = process.env.PORT || 5000;
 const ventRoutes = require("./routes/ventRoutes");
 
 const express = require("express");
@@ -13,8 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.log(err));
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
 app.use("/api/auth", authRoutes);
 
@@ -26,6 +26,6 @@ app.get("/", (req, res) => {
   res.send("CampusMind API Running");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
